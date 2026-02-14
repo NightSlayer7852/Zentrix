9e82e19486a208e17f999d7716063918eeb781cd
NightSlayer7852 <tanawadepranav@gmail.com>
2026-02-14 07:43:55 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Accordion } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <Accordion type="single" collapsible>
+      Hello World
+    </Accordion>
+  );
+}
