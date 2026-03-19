62b40e89c738dec68bceee78e49c4afa4955efc8
NightSlayer7852 <186535326+NightSlayer7852@users.noreply.github.com>
2026-03-19 14:01:24 +0530

    Refactor Layout component to use ProjectLayoutProps

--- src/app/projects/[projectId]/layout.tsx
+++ src/app/projects/[projectId]/layout.tsx
@@ -1,14 +1,28 @@
-
 import { Id } from "../../../../convex/_generated/dataModel";
 import { ProjectIdLayout } from "@/features/projects/components/projectid-layout";
-const Layout = async ({ children, params }: { children: React.ReactNode; params: { projectId: Id<"projects"> } }) => {
-    const { projectId } = await params;
-    return (
-        <ProjectIdLayout projectId={projectId}>
-            {children}
-        </ProjectIdLayout>
-    )
 
-}
+type ProjectLayoutProps = {
+  children: React.ReactNode;
+  params: Promise<{ projectId: string }>;
+};
 
-export default Layout;  
+const Layout = async ({ children, params }: ProjectLayoutProps) => {
+  const { projectId } = await params;
+
+  return <ProjectIdLayout projectId={projectId as Id<"projects">}>{children}</ProjectIdLayout>;
+};
+
+export default Layout;
+// import { Id } from "../../../../convex/_generated/dataModel";
+// import { ProjectIdLayout } from "@/features/projects/components/projectid-layout";
+// const Layout = async ({ children, params }: { children: React.ReactNode; params: { projectId: Id<"projects"> } }) => {
+//     const { projectId } = await params;
+//     return (
+//         <ProjectIdLayout projectId={projectId}>
+//             {children}
+//         </ProjectIdLayout>
+//     )
+
+// }
+
+// export default Layout;  
