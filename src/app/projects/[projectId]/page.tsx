3db26a21ea558854162a22239e34dcc27ca622f4
NightSlayer7852 <186535326+NightSlayer7852@users.noreply.github.com>
2026-03-19 14:02:03 +0530

    Refactor ProjectIdPage to use typed parameters

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,12 +1,24 @@
 import { Id } from "../../../../convex/_generated/dataModel";
 import ProjectIdView from "@/features/projects/components/projectid-view";
 
-const ProjectIdPage = async({ params }: { params: Promise<{ projectId: Id<"projects"> }> }) => {
-    const { projectId } = await params;
-    
-    return (
-        <ProjectIdView projectId={projectId} />
-    )
-}
+type ProjectPageProps = {
+  params: Promise<{ projectId: string }>;
+};
+
+const ProjectIdPage = async ({ params }: ProjectPageProps) => {
+  const { projectId } = await params;
+
+  return <ProjectIdView projectId={projectId as Id<"projects">} />;
+};
 
 export default ProjectIdPage;
+
+// const ProjectIdPage = async({ params }: { params: Promise<{ projectId: Id<"projects"> }> }) => {
+//     const { projectId } = await params;
+    
+//     return (
+//         <ProjectIdView projectId={projectId} />
+//     )
+// }
+
+// export default ProjectIdPage;
